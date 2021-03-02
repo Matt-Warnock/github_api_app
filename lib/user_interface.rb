8db24316c09a504b426b7f59bc531e272a8f65f7
MattW <mattnwarnock@yahoo.co.uk>
2021-03-02 16:42:37 +0000

    Create tests and pass for collecting vaid username

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,1 +1,22 @@
 # frozen_string_literal: true
+
+class UserInterface
+  def initialize(input, output)
+    @input = input
+    @output = output
+  end
+
+  def ask_for_valid_username
+    loop do
+      output.print 'Please enter a github username: '
+      username = input.gets.chomp
+      break username if username.match?(/^[\w](?:[\w]|-(?=[\w])){0,38}$/i)
+
+      output.print "Invalid Git-Hub username\n"
+    end
+  end
+
+  private
+
+  attr_reader :input, :output
+end
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -1,1 +1,41 @@
 # frozen_string_literal: true
+
+require 'user_interface'
+
+RSpec.describe UserInterface do
+  describe '#ask_for_valid_username' do
+    let(:input) { StringIO.new("Matt-Warnock\n") }
+    let(:output) { StringIO.new }
+    let(:ui) { described_class.new(input, output) }
+
+    it 'asks user for github username' do
+      ui.ask_for_valid_username
+
+      expect(output.string).to include('Please enter a github username: ')
+    end
+
+    it 'collects valid github username from user' do
+      result = ui.ask_for_valid_username
+
+      expect(result).to eq('Matt-Warnock')
+    end
+
+    it 'prints error message if username is invaild' do
+      input = StringIO.new("-mattwarnock\nmatt-warnock\n")
+      ui = described_class.new(input, output)
+
+      ui.ask_for_valid_username
+
+      expect(output.string).to include('Invalid Git-Hub username')
+    end
+
+    it 'keeps asking user for input untill a valid username is given' do
+      input = StringIO.new("matt--warnock\nmatt-warnock\n")
+      ui = described_class.new(input, output)
+
+      ui.ask_for_valid_username
+
+      expect(output.string.scan('Please enter').length).to eq(2)
+    end
+  end
+end
