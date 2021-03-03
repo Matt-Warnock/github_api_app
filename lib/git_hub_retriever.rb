4e2a98f4716d5c98e009879b765a705d8c143fc6
MattW <mattnwarnock@yahoo.co.uk>
2021-03-03 20:25:56 +0000

    Pass tests for json parse

--- lib/git_hub_retriever.rb
+++ lib/git_hub_retriever.rb
@@ -2,9 +2,11 @@
 
 require 'rest-client'
 
-class GitHubRetriever
+class GitHubClient
   def collect_user_details(username)
     uri = URI.parse("api.github.com/users/#{username}")
-    RestClient.get(uri.path)
+    responce = RestClient.get(uri.path)
+
+    JSON.parse(responce, { symbolize_names: true }) if responce.code == 200
   end
 end
--- spec/git_hub_retriever_spec.rb
+++ spec/git_hub_retriever_spec.rb
@@ -8,12 +8,35 @@ RSpec.describe GitHubRetriever do
 
   describe '#collect_user_details' do
     it 'sends a get request to the github usernames api address' do
-      stub = stub_request(:get, 'http://api.github.com/users/Matt-Warnock')
-             .to_return(status: 200, body: user_details)
+      stub = ok_stub
 
       retriever.collect_user_details('Matt-Warnock')
 
       expect(stub).to have_been_requested
     end
+
+    it 'returns a hash from the github api if status is 200' do
+      user_details_hash = JSON.parse(user_details, { symbolize_names: true })
+      ok_stub
+
+      result = retriever.collect_user_details('Matt-Warnock')
+
+      expect(result).to eq(user_details_hash)
+    end
+
+    it 'it does not try to return a hash if status is anything other than 200' do
+      user_details_hash = JSON.parse(user_details, { symbolize_names: true })
+      stub_request(:get, 'http://api.github.com/users/Matt-Warnock')
+        .to_return(status: 204)
+
+      result = retriever.collect_user_details('Matt-Warnock')
+
+      expect(result).to_not eq(user_details_hash)
+    end
+  end
+
+  def ok_stub
+    stub_request(:get, 'http://api.github.com/users/Matt-Warnock')
+      .to_return(status: 200, body: user_details)
   end
 end
