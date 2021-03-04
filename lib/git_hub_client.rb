3937e165b10c9110bd421b88d12d8a0e08e28e77
Matt-Warnock <60718290+Matt-Warnock@users.noreply.github.com>
2021-03-04 16:50:59 +0000

    Create requester (#3)
    Create github client

---
+++ lib/git_hub_client.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rest-client'
+
+class GitHubClient
+  def collect_user_details(username)
+    uri = URI.parse("api.github.com/users/#{username}")
+
+    RestClient.get(uri.path) do |responce|
+      return json_to_hash(responce.body) if responce.code == 200
+
+      false
+    end
+  end
+
+  private
+
+  def json_to_hash(responce)
+    JSON.parse(responce, { symbolize_names: true })
+  rescue JSON::ParserError
+    false
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'coveralls'
+require 'webmock/rspec'
 Coveralls.wear!
 
 require 'pry'
